0b51964790abd573b51941a1f5379cea5d41f206
Junel Solis <junel.solis@abo.fi>
2023-12-04 12:17:58 +0200

    implement tubule classification according to updated data structure from api

--- src/src/components/output/TubuleClassification.vue
+++ src/src/components/output/TubuleClassification.vue
@@ -4,10 +4,25 @@ import { useTaskStore } from '../../stores/task'
 
 const store = useTaskStore()
 const selectedImageFilename = computed(() => store.task.image_filenames[store.selectedImageIndex])
-const tubData = computed(() => store.task.results[selectedImageFilename.value]['tub_data'])
-const tubCount = computed(() => tubData.value.boxes.length)
-const tubClasses = computed(() => tubData.value.labels)
-const tubScores = computed(() => tubData.value.scores)
+
+const imageData = computed(() => store.task.results[selectedImageFilename.value])
+const tubData = computed(() => imageData.value.tubules)
+const tubCount = computed(() => tubData.value.length)
+const tubClasses = computed(() => imageData.value.tubules.map((tub) => tub.label))
+const tubScores = computed(() => imageData.value.tubules.map((tub) => tub.score))
+const tubIds = computed(() => imageData.value.tubules.map((tub) => tub.id))
+
+const tubuleObjects = computed(() => {
+  const tubules = []
+  for (let i = 0; i < tubCount.value; i++) {
+    tubules.push({
+      class: tubClasses.value[i],
+      score: tubScores.value[i],
+      id: tubIds.value[i]
+    })
+  }
+  return tubules
+})
 </script>
 <script lang="ts">
 export default {
@@ -16,12 +31,12 @@ export default {
 </script>
 <template>
   <div class="tubule-classification">
-    <h2 class='font-semibold text-lg'>
+    <h2 class="font-semibold text-lg">
       Tubules <span class="italic">({{ tubCount }})</span>
     </h2>
     <ul>
-      <li v-for="(tubClass, idx) in tubClasses" :key="idx">
-        Tubule {{ idx + 1 }}: {{ tubClass }} ({{ (parseFloat(tubScores[idx]) * 100).toFixed(2) }}%)
+      <li v-for="tub in tubuleObjects" :key="tub.id">
+        Tubule {{ tub.id }}: {{ tub.class }} ({{ (parseFloat(tub.score) * 100).toFixed(2) }}%)
       </li>
     </ul>
   </div>
