8e74e2f0dda6804873c0cbff4a9c819d613347fe
Junel Solis <junel.solis@abo.fi>
2024-03-20 13:44:33 +0200

    tubule stage overlays: - overlay component disables scrollbars in lower page elements - Emits 'close-overlay' event when button clicked - Implement stubs of tubule stage overlays - Implement Stage I overlay

--- src/components/tubule-overlays/StageOneContentOverlay.vue
+++ src/components/tubule-overlays/StageOneContentOverlay.vue
@@ -1,5 +1,26 @@
 <template>
-  <overlay-info>Stage I content</overlay-info>
+  <overlay-info>
+    <h1>Stages I-V</h1>
+    <div class="image-div">
+      <ul>
+        <li>
+          <img src="~@/assets/tubule-stages/1.jpg" alt="" />
+          <p>Stage I</p>
+        </li>
+        <li>
+          <img src="~@/assets/tubule-stages/2_5.jpg" alt="" />
+          <p>Stage II-V</p>
+        </li>
+      </ul>
+    </div>
+    <div class="mt-6">
+      <h2>Criteria</h2>
+      <p>
+        Presence of Spg, pachytene Spc, RS and late condensed elSpt which are bundles. Pachytene Spc
+        are relatively small (I-IV) but gradually grow in size towards Stage V.
+      </p>
+    </div>
+  </overlay-info>
 </template>
 <script lang="ts">
 import OverlayInfo from '../OverlayInfo.vue'
