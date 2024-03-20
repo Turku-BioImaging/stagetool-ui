30ef5d7c0e62cacf861b34345f1920dd274cfe92
dadotbi <dado.d.tokic@utu.fi>
2024-03-20 23:31:35 +0200

    uploaded images

--- src/views/CellModel.vue
+++ src/views/CellModel.vue
@@ -1,9 +1,16 @@
 <template>
   <div class="cell-model-test">
-    <p>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto, unde tempore labore
-      soluta quidem deserunt incidunt sequi, ut laborum quae eos iusto libero provident nam!
-      Accusantium temporibus ab nobis nulla?
+  <p>
+    Cell categories recognized by STAGETOOL are:<br>
+      1. Sertoli cells (Sert)<br>
+      2. Spermatogonia (Spg), including type A, intermediate, and type B spermatogonia.<br>
+      3. Early primary spermatocytes (Spc-prePach), including preleptotene, leptotene, and zygotene spermatocytes<br>
+      4. Mid-to-late primary spermatocytes (Spc-Pach), including pachytene and diplotene spermatocytes<br>
+      5. Meiotic divisions (m2m)<br>
+      6. Secondary spermatocytes (Spc-sec)<br>
+      7. Round spermatids (RS)<br>
+      8. Early elongating spermatids (elSpt-early)<br>
+      9. Late elongating spermatids (elSpt-late)<br>
     </p>
   </div>
 </template>
