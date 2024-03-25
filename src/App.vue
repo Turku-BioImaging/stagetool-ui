681cf77d0543bb787ad4923f423a3a2408f60528
Junel Solis <junel.solis@abo.fi>
2024-03-25 15:03:25 +0200

    Home background image: - added bg image from sample images - low transparency and fixed - adjustments to navbar hiding behavior

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,41 @@
 import { RouterLink, RouterView } from 'vue-router'
 import NavigationBar from './components/NavigationBar.vue'
 </script>
+<script lang="ts">
+export default {
+  data() {
+    return {
+      menuIsOpen: true
+    }
+  },
+  methods: {
+    checkScroll() {
+      if (this.$router.currentRoute.value.path === '/' && window.scrollY <= 200) {
+        this.menuIsOpen = false
+      } else {
+        this.menuIsOpen = true
+      }
+    }
+  },
+  mounted() {
+    window.addEventListener('scroll', this.checkScroll)
+    this.checkScroll()
+  },
+  beforeDestroy() {
+    window.removeEventListener('scroll', this.checkScroll)
+  }
+}
+</script>
 
 <template>
   <div class="app">
-    <navigation-bar/>
+    <navigation-bar v-show="menuIsOpen" />
 
-    <div class="clear-div"></div>
-    <RouterView />
+    <RouterView class="pt-24" />
   </div>
 </template>
 
-<style lang='scss'>
+<style lang="scss">
 div.app {
   // height: 100vh;
 
