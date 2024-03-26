1154abe39f6ae8b0c3e18eab090c7fac97a5c8c5
Junel Solis <junel.solis@abo.fi>
2024-03-26 12:01:34 +0200

    lg layouts for home page

--- src/components/SampleImageGrid.vue
+++ src/components/SampleImageGrid.vue
@@ -13,7 +13,8 @@ export default {
 <template>
   <div class="sample-image-grid">
     <div class="image-grid-container">
-      <h2>Try a demo image</h2>
+      <h1>Try StageTool</h1>
+      <h2>Choose a demo image</h2>
       <ul>
         <li><img @click="sendImageSrc" src="@/assets/sample-images/01.png" alt="" /></li>
         <li><img @click="sendImageSrc" src="@/assets/sample-images/02.png" alt="" /></li>
@@ -30,6 +31,9 @@ div.sample-image-grid {
   @apply p-6;
 
   div.image-grid-container {
+    h1 {
+      @apply text-center text-cyan-500 text-3xl;
+    }
     h2 {
       @apply text-2xl text-center mb-4;
     }
@@ -37,7 +41,17 @@ div.sample-image-grid {
       @apply grid grid-cols-3 gap-2;
       li {
         @apply border border-slate-700;
+
+        &:hover {
+          @apply border-cyan-500 border-4;
+        }
       }
+    }
+  }
+
+  @screen lg {
+    div.image-grid-container {
+      @apply w-2/3 mx-auto my-16;
     }
   }
 }
