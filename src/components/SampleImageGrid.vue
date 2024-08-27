e1b9b8e51738f74e9eccb817876a6b19da7c7041
Junel Solis <junel.solis@abo.fi>
2024-08-27 15:14:08 +0300

    update sample grid

--- src/components/SampleImageGrid.vue
+++ src/components/SampleImageGrid.vue
@@ -12,47 +12,56 @@ export default {
 </script>
 <template>
   <div class="sample-image-grid">
-    <div class="image-grid-container">
-      <h1>Try StageTool</h1>
-      <h2>Choose a demo image</h2>
-      <ul>
-        <li><img @click="sendImageSrc" src="@/assets/sample-images/01.png" alt="" /></li>
-        <li><img @click="sendImageSrc" src="@/assets/sample-images/02.png" alt="" /></li>
-        <li><img @click="sendImageSrc" src="@/assets/sample-images/03.png" alt="" /></li>
-        <li><img @click="sendImageSrc" src="@/assets/sample-images/04.png" alt="" /></li>
-        <li><img @click="sendImageSrc" src="@/assets/sample-images/05.png" alt="" /></li>
-        <li><img @click="sendImageSrc" src="@/assets/sample-images/06.png" alt="" /></li>
-      </ul>
+    <div class="container mx-auto">
+      <div class="image-grid">
+        <h2>Try StageTool</h2>
+        <p >
+          Choose one of the demo images to see how StageTool works. The selected image is sent to a
+          server for artificial intelligence prediction using the StageTool tubule and cell models.
+          The results will be shown in a separate page.
+        </p>
+        <ul class="mt-8">
+          <li><img @click="sendImageSrc" src="@/assets/sample-images/01.png" alt="" /></li>
+          <li><img @click="sendImageSrc" src="@/assets/sample-images/02.png" alt="" /></li>
+          <li><img @click="sendImageSrc" src="@/assets/sample-images/03.png" alt="" /></li>
+          <li><img @click="sendImageSrc" src="@/assets/sample-images/04.png" alt="" /></li>
+          <li><img @click="sendImageSrc" src="@/assets/sample-images/05.png" alt="" /></li>
+          <li><img @click="sendImageSrc" src="@/assets/sample-images/06.png" alt="" /></li>
+        </ul>
+        <p class="mt-6 text-sm italic">Compute resources for this demo are provided by the <a href="https://csc.fi/en/" target="_blank">Centre for Scientific Computing</a>.</p>
+      </div>
     </div>
   </div>
 </template>
-<style lang="scss">
+<style lang="scss" scoped>
 div.sample-image-grid {
-  @apply p-6;
+  // @apply p-6;
 
-  div.image-grid-container {
-    h1 {
-      @apply text-center text-cyan-500 text-3xl;
+  div.image-grid {
+    @screen lg {
+      @apply w-3/4 mx-auto py-12;
     }
+
     h2 {
-      @apply text-2xl text-center mb-4;
+      // @apply text-center text-cyan-500 text-3xl;
+      @apply font-semibold uppercase tracking-widest text-3xl;
     }
     ul {
-      @apply grid grid-cols-3 gap-2;
+      @apply grid grid-cols-6 gap-4;
       li {
         @apply border border-slate-700;
 
         &:hover {
-          @apply border-cyan-500 border-4;
+          @apply border-rose-500 border-4;
         }
       }
     }
   }
 
-  @screen lg {
-    div.image-grid-container {
-      @apply w-2/3 mx-auto my-16;
-    }
-  }
+  // @screen lg {
+  //   div.image-grid-container {
+  //     @apply w-2/3 mx-auto my-16;
+  //   }
+  // }
 }
 </style>
