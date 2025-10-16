fbde0c6ce0591e6360007c83751c58b897a0abf2
Chris Buehling <chris.buehling@web.de>
2025-10-16 14:07:51 +0300

    * add description

--- src/components/output/DownloadFunctionality.vue
+++ src/components/output/DownloadFunctionality.vue
@@ -1,3 +1,5 @@
+<!-- This is the description for the user on how to download the resulting image. The actual download functionality is the a-tag in VisualizationViewer.vue. --> 
+
 <template>
   <div class="download-component">
     Click on the image to download.
