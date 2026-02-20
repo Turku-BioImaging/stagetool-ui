e2658286fe943493cfba2918f7707d568797e7cc
chris-buehling <chris.buhling@abo.fi>
2026-02-20 14:39:18 +0200

    * add filename in visualisation again * sizing of error field

--- src/components/output/VisualizationViewer.vue
+++ src/components/output/VisualizationViewer.vue
@@ -20,15 +20,16 @@ export default {
       <div class="vis-viewer-component" style="display: inline-block" v-if="!store.task?.visualization_filenames?.[n-1].startsWith('pred_')">
         <a :download="['result', store.task?.image_filenames?.[n-1]].join('-')" :href=store.task?.visualization_sources?.[n-1] style="width: 100%" v-if="!store.task?.visualization_filenames?.[n-1].endsWith('error.txt')">
           <img :src="store.task?.visualization_sources?.[n-1]" alt="" width="1024" height="1024"/>
+          <p class="mt-2 font-semibold text-xs text-center">
+            {{ ['result', store.task?.image_filenames?.[n-1]].join('-') }}
+          </p>
         </a>
         <div width="1024" height="1024" v-else>
           Image {{ store.task?.image_filenames?.[n-1] }} could not be processed. The corresponding error is:
-          <object :data="store.task?.visualization_sources?.[n-1]" alt="" width="1024" height="1024">
+          <object :data="store.task?.visualization_sources?.[n-1]" alt="" >
             Could not read the error message.
           </object>
         </div>
-        <p class="mt-2 font-semibold text-xs text-center">
-        </p>
       </div>
     </template>
   </div>
